16c06b90b2035e04dc1a6a1126e6a3e6384ab206
Eric Howard <40253104+EricHoward27@users.noreply.github.com>
2023-04-25 19:11:29 -0500

    add stripeid to user; create event for user

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import Stripe from "stripe";
 
 const prisma = new PrismaClient();
 
@@ -13,4 +14,25 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  //create a event to update database with stripe customer id after a user is created
+  events: {
+    createUser: async ({user}) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15",
+      });
+      //Lets create a customer in stripe
+      if(user.name && user.email) {
+      const customer = await stripe.customers.create({
+        email: user.email,
+        name: user.name,
+      });
+   
+      //update the user in the database with the stripeId
+      await prisma.user.update({
+        where: {id: user.id},
+        data: {stripeCustomerId: customer.id},
+      });
+    }
+  },
+},
 });
