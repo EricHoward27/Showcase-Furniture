267e7904cf9ffc98f108521f0a5a6816f4cbcc35
Eric Howard <40253104+EricHoward27@users.noreply.github.com>
2023-05-01 21:33:06 -0500

    format stripe price;util format; test data;

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import formatPrice from "@/util/PriceFormat";
 
 interface ProductProps {
     name: string;
@@ -11,7 +12,7 @@ export default function Product({name, price, image}: ProductProps) {
     <div>
         <Image src={image} alt={name} width={400} height={400} />
       <h1>{name}</h1>
-      <h1>{price}</h1>
+      <h1>{price !== null ? formatPrice(price): "N/A"}</h1>
     </div>
   )
 }
