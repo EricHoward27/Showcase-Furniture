d8f024869cac1bfb62f38aefb039b9590465c285
Eric Howard <40253104+EricHoward27@users.noreply.github.com>
2023-05-16 09:19:52 -0500

    install framer motion; animated cart icon in nav component;animated main divs in cart component; test feature;

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -4,7 +4,7 @@ import { useCartStore } from "@/store";
 import formatPrice from "@/util/PriceFormat";
 import {IoAddCircleOutline, IoRemoveCircle} from "react-icons/io5";
 import basket from "../../public/images/shopping-basket.png";
-
+import { motion, AnimatePresence } from "framer-motion";
 
 const Cart = () => {
     const cartStore = useCartStore();
@@ -14,11 +14,23 @@ const Cart = () => {
         return acc + item.unit_amount! * item.quantity!
     }, 0 );
     return (
-        <div onClick={() => cartStore.toggleCart()} className="fixed w-full h-screen left-0 top-0 bg-black/25">
-            <div onClick={(e) => e.stopPropagation()} className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700">
-                <h1>Shopping Cart Test</h1>
+        <motion.div 
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        exit={{opacity: 0}}
+        onClick={() => cartStore.toggleCart()} 
+        className="fixed w-full h-screen left-0 top-0 bg-black/25">
+            {/* Cart */}
+            <motion.div 
+            layout
+            onClick={(e) => e.stopPropagation()} 
+            className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700">
+                <h1>Cart</h1>
                 {cartStore.cart.map((item) => (
-                    <div key={item.id} className="flex py-4 gap-4">
+                    <motion.div 
+                    layout
+                    key={item.id} 
+                    className="flex py-4 gap-4">
                         <Image className="rounded h-24" src={item.image ?? ""} alt={item.name} width={120} height={120} />
                         <div>
                             <h2>{item.name}</h2>
@@ -45,24 +57,33 @@ const Cart = () => {
                             <p className="text-sm">{formatPrice(item.unit_amount)}</p>
                            
                         </div>
-                    </div>
+                    </motion.div>
                 ))}
                 {/* Total Price and check logic if cart empty total price dont render */}
                 {cartStore.cart.length > 0 && (
                     <p>Total: {formatPrice(totalPrice)}</p>
                 )}
                 {/** if cart is empty btn will not rendered */}
+                <motion.div layout>
                 {cartStore.cart.length > 0 && (
                    <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">Checkout</button>     
                 )}
+                </motion.div>
+                {/* if cart is empty show this; add animation for page when cart empty */}
+                <AnimatePresence>
                 {!cartStore.cart.length && (
-                    <div className="flex flex-col items-center gap-12 text 2xl font-medium pt-56 opacity-75">
+                    <motion.div 
+                    animate={{scale: 1, rotateZ: 0, opacity: 0.75}}
+                    initial={{scale: 0.5, rotateZ: -10, opacity: 0}}
+                    exit={{scale: 0.5, rotateZ: -10, opacity: 0}}
+                    className="flex flex-col items-center gap-12 text 2xl font-medium pt-56 opacity-75">
                         <h1>Your cart is currently empty </h1>
                         <Image src={basket} alt="basket" width={200} height={200} />
-                    </div>
+                    </motion.div>
                 )}
-            </div>
-        </div>
+                </AnimatePresence>
+            </motion.div>
+        </motion.div>
     )
 }
 export default Cart;
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -7,6 +7,7 @@ import showcaseLogo from "../../public/images/showcase_logo.png";
 import Cart from "./Cart";
 import { useCartStore } from "@/store";
 import {CgShoppingCart} from "react-icons/cg";
+import {motion, AnimatePresence} from "framer-motion";
 
 // create a Nav component
 const Nav = ({ user }: Session) => {
@@ -23,9 +24,18 @@ const Nav = ({ user }: Session) => {
                     { /*cart icon & toggle the cart */}
                     <li onClick={() => cartStore.toggleCart()} className=" flex items-center text-3xl relative cursor-pointer">
                         <CgShoppingCart />
-                        <span className="bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex items-center justify-center ">
+                        {/* if cart is empty, hide the number */}
+                        <AnimatePresence>
+                        {cartStore.cart.length > 0 && (
+                        <motion.span 
+                            animate={{scale: 1}} 
+                            initial={{scale: 0}} 
+                            exit={{scale: 0}}
+                            className="bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex items-center justify-center ">
                             {cartStore.cart.length}
-                        </span>
+                        </motion.span>
+                        )}
+                        </AnimatePresence>
                     </li>
                     {/* if user is not log in */} 
                     {!user && (
@@ -51,7 +61,9 @@ const Nav = ({ user }: Session) => {
                 </ul>
                 </div>
             </div>
-            {cartStore.isOpen && <Cart />}
+            <AnimatePresence>
+                {cartStore.isOpen && <Cart />}          
+            </AnimatePresence>
         </nav>
      );
 }
